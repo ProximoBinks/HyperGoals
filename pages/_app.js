f3e2170a335a321098044ead866e84c63ec082fa
Elliot Koh <109998783+ProximoBinks@users.noreply.github.com>
2025-03-01 18:10:32 +1030

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,15 @@ import Layout from '@components/Layout'
 function Application({ Component, pageProps }) {
   return (
     <>
-    <Head>
-        <link rel="icon" href="/favicon.ico" />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icons/favicon.ico" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="theme-color" content="#111827" />
       </Head>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   )
 }
